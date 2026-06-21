ffba6766e6446842c73d85ef68e9d067cf00acad
mawwka <166003344+rewrrry@users.noreply.github.com>
2026-06-22 07:33:43 +1000

    Create task3.js

---
+++ task3.js
@@ -0,0 +1,16 @@
+let a = 2;
+let b = 6;
+let sign = "*";
+
+let result3;
+if (sign === "+") {
+    result3 = a + b;
+} else if (sign === "-") {
+    result3 = a - b;
+} else if (sign === "/") {
+    result3 = a / b;
+} else if (sign === "*") {
+    result3 = a * b;
+}
+
+console.log(result3);
